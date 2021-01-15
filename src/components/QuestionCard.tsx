f25e39f9d92f02379f355d2031892e2bfec67201
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-01-15 09:42:39 -0600

    refracted code

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import { AnswerObject } from '../App';
+
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
+
 type Props = {
     question: string;
     answers: string[];
-    callback: any;
-    userAnswer: any;
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+    userAnswer: AnswerObject | undefined;
     questionNr: number;
     totalQuestions: number;
 }
@@ -16,20 +20,26 @@ callback,
 userAnswer, 
 questionNr, 
 totalQuestions
-}) => (<div>
-    <p className="number">
-        Question: {questionNr} / {totalQuestions}
+}) => (
+<Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
     </p>
-    <p dangerouslySetInnerHTML = {{__html: question}} />
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-    {answers.map(answer => (
-        <div>
-            <button disabled={userAnswer} onClick={callback}>
-                <span dangerouslySetInnerHTML={{__html: answer}}/>
-            </button>
-            </div>
-    ))}
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
+          <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
+          </button>
+        </ButtonWrapper>
+      ))}
     </div>
-</div>);
+  </Wrapper>
+);
 
 export default QuestionCard;
