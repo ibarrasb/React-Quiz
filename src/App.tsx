6a084279adfa613bfa1a8c3ee5634ae0e3930252
Eduardo Ibarra <eddieibarra43@gmail.com>
2021-01-17 19:37:40 -0600

    finished app

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const App: React.FC = () => {
     <>
       <GlobalStyle />
       <Wrapper>
-        <h1>REACT QUIZ</h1>
+        <h1>Are You Smarter Than A Programmer?</h1>
         {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
           <button className='start' onClick={startTrivia}>
             Start
